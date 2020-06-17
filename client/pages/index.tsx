79bf8c2d6153c707acf867c5c6797edba9913acd
Dmytro <dimasoltusyuk@gmail.com>
2020-06-17 13:54:13 +0300

    Add files via upload

---
+++ client/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import { Button } from 'antd'
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Button>Button</Button>
+    </div>
+  )
+}
