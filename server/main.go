230cf58fc292472c904efb32fa2d0ac75235b1d4
Dmytro <dimasoltusyuk@gmail.com>
2020-06-18 18:06:10 +0300

    nuxt + go update

--- server/main.go
+++ server/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"./handler"
+
+	"log"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", handler.Welcome())
+
+	if err := e.Start(":3000"); err != nil {
+		log.Fatalln(err)
+	}
+}
